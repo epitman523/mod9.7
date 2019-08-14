4693f29bf5bfa022da12555af876615ef4eb5865
Elizabeth Emig <epitman523@gmail.com>
2019-08-14 09:05:44 -0500

    built watchForm, getdata and format userState functions

--- scripts.js
+++ scripts.js
@@ -1,5 +1,32 @@
 'use strict'
 
+const baseURL = 'https://developer.nps.gov/api/v1/';
+const apiKey = 'a4XGjvJiyzsKuEWxfhRO9WtObErCGID9qffSqElG';
+
+function getParkData(query, maxResult = 10) {
+    const params = {
+        stateCode: query,
+        limit: maxResult,
+        fields: addresses,
+        api_key: apiKey
+    };
+
+}
+function formatUserState(string) {
+    const stateArr = string.toUpperCase().replace(/\s/g, '').split(',');
+    return stateArr;
+}
+function watchForm() {
+    $('#searchParks').submit(event => {
+        event.preventDefault();
+        const userState = formatUserState($('#js-search-term').val());
+        const userMax = $('#js-max-results').val();
+        console.log('watchForm is working');
+    });
+}
+
+$(watchForm());
+
 //1. Watch for for submission
 //2. On submit capture 2 values
     //2.1 State
@@ -11,5 +38,3 @@
 //5. Create query string for link
 //6. Fetch data from API
 //7. Display data
-
-//$(watchForm());
