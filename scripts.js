5269945ed877b2bce4f96de778f3a5323859342b
Elizabeth Emig <epitman523@gmail.com>
2019-08-14 07:28:51 -0500

    tweaked results input, set up plan in js

---
+++ scripts.js
@@ -0,0 +1,15 @@
+'use strict'
+
+//1. Watch for for submission
+//2. On submit capture 2 values
+    //2.1 State
+    //2.2 Max Results
+//3. Send both values to a get function
+//4. Send state value through formatting function
+    //4.1 Split string value into array at commas
+    //4.2 Any other filters necessary?
+//5. Create query string for link
+//6. Fetch data from API
+//7. Display data
+
+//$(watchForm());
